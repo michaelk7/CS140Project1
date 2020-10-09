96bb7415fb6db42fdeb7e23175db1c3d1b97010b
Michael Kelnar <michaelkelnar7@gmail.com>
2020-10-09 03:37:35 -0500

    Completed encryption method and spaces method
    Spaces method still needs to be debugged

--- project1/project1/main.cpp
+++ project1/project1/main.cpp
@@ -10,42 +10,13 @@
 #include <string>
 #include <random>
 #include <ctime>
+using namespace std;
 
-//constant variables
-int space = 5;
-
-int main()
-{
-//    (Intro)
-//    (Ask for choice)
-//      if (choice 1)
-//        - user enters key they want to be encrypted
-//        - valid input (function)
-//        - if invalid, display error message
-//        - if valid, convert to uppercase function
-//        - ask for plaintext
-//        - ask for # of spaces
-//        - execute other functions
-//        - output final encryted text
-//      if (choice 2)
-//        - random key function
-//        - ask for plaintext
-//        - ask for # of spaces
-//        - execute functions
-//        - output final encrypted text
-    
-}
-
-bool isValid(std::string input)
-{
-//    - only supposed to contain letters
-//        - isalpha() function we could use on strings
-//    - check for same letters in consecutive order
-//    - check for length (26 max)
-    
-    
-    return false;
-}
+string generateRandomKey();
+bool isValid(string);
+string key;
+int spaces;
+string normalAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 std::string convertToUpperCase(std::string input)
 {
@@ -54,40 +25,6 @@ std::string convertToUpperCase(std::string input)
     return input;
 }
 
-std::string encryption(std::string input)
-{
-//  - each letter in key corresponds to a letter in the alphabet
-//  - each letter in plaintext corresponds to each letter in key
-//      ex. ZHSRWOTEVXQPIABGYUCLFNJMDK
-//          ||||||||||||....
-//          ABCDEFGHIJKLMNOPQRSTUVWXYZ
-//          plaintext: THE Time Traveler
-//          THE Time Traveler
-//          ||| |||| ||||||||
-//          LEW LVIW ....
-//   - I think it is easier to encrypt it first then do the spacing later
-    std::string output;
-    
-    //code
-    
-    return output;
-}
-
-std::string spacing(std::string finalMessage, int space)
-{
-//  - now split final message into the specified spaces
-    std:: string output;
-    
-    //code
-    
-    return output;
-}
-
-//The following is a generateRandomKey() function that you may use in Project 1
-//Note: you will likely need to #include <random> and #include <ctime>
-
-
-//generates an uppercase MASC key by shuffling the English alphabet
 std::string generateRandomKey()
 {
     std::string randomKey{ "ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
@@ -100,3 +37,140 @@ std::string generateRandomKey()
 
     return randomKey;
 }
+
+bool isValid(string input) {
+
+    // checking for max length
+    if (input.length() > 26) {
+        cout << "Incorrect key length. Must be 26 characters." << endl;
+    }
+    else {
+
+        for (int i{ 0 }; i < input.size(); i++) {
+
+            // checking for non-alphabetic characters
+            if (!isalpha(input.at(i))) {
+                cout << "Error: invalid character in key." << endl;
+                break;
+            }
+
+            // checking for same consecutive letters
+            if (input.at(i) == input.at(i + 1)) {
+                cout << "Error: letters in key must be used only once." << endl;
+                break;
+            }
+        }
+
+    }
+    return false;
+}
+
+string encryption(string input, string key)
+{
+//  - each letter in key corresponds to a letter in the alphabet
+//  - each letter in plaintext corresponds to each letter in key
+//      ex. ZHSRWOTEVXQPIABGYUCLFNJMDK
+//          ||||||||||||....
+//          ABCDEFGHIJKLMNOPQRSTUVWXYZ
+//          plaintext: THE Time Traveler
+//          THE Time Traveler
+//          ||| |||| ||||||||
+//          LEW LVIW ....
+//   - I think it is easier to encrypt it first then do the spacing later
+    string output;
+    
+    for(int i = 0; i < input.length(); i++)
+    {
+        long temp = normalAlphabet.find(input.at(i));
+        output += key[temp];
+    }
+    return output;
+}
+
+string spacing(string finalMessage, int spaces)
+{
+    string output;
+    
+    for(int i = 0; i < finalMessage.length(); i++)
+    {
+        if(i % spaces == 0)
+        {
+            output += " ";
+        }else
+        {
+            output += finalMessage.at(i);
+        }
+    }
+    
+    return output;
+}
+
+
+int main() {
+
+    int choice;
+    string plaintext;
+    string encryptedText;
+    
+    cout << "Testing Monoalphabetic Substitution Cipher (MASC) program:" << endl;
+    cout << endl;
+    cout << "Please make a selection:" << endl;
+    cout << "(1) Type in a key" << endl;
+    cout << "(2) Have a key generated for you" << endl;
+    cout << endl;
+
+    // asking user for their choice
+    cout << "Choice: ";
+    cin >> choice;
+    cin.ignore();
+    cout << endl;
+
+    if (choice == 1) {
+        
+        cout << "Enter a key to be used for encryption and decryption. Include each letter of alphabet, none repeated:" << endl;
+        getline(cin, key);
+        isValid(key);
+        transform(key.begin(), key.end(), key.begin(), ::toupper);
+        cout << "Key is now: " << key << endl;
+        cout << endl;
+
+        cout << "Enter the plaintext: ";
+        getline(cin, plaintext);
+        cout << endl;
+
+        cout << "We will now disguise the original spacing." << endl;
+        cout << "How many letters should appear in each grouping? " << '\n' << "(press Enter for default spacing of 5, negative entry leaves original spacing" << endl;
+        cout << "Spacing: ";
+        cin >> spaces;
+        cout << endl;
+        plaintext = convertToUpperCase(plaintext);
+        // call encryption() function here
+        encryptedText = encryption(plaintext, key);
+        // call spacing() function here
+        encryptedText = spacing(encryptedText, spaces);
+        cout << "Ciphertext is: " + encryptedText << endl;
+    }
+    else if (choice == 2) {
+        key = generateRandomKey();
+        cout << "Key is now: " << key << endl;
+        cout << endl;
+        cout << "Enter the plaintext: ";
+        getline(cin, plaintext);
+        cout << endl;
+
+        cout << "We will now disguise the original spacing." << endl;
+        cout << "How many letters should appear in each grouping? " << '\n' << "(press Enter for default spacing of 5, negative entry leaves original spacing" << endl;
+        cout << "Spacing: ";
+        cin >> spaces;
+        cout << endl;
+        plaintext = convertToUpperCase(plaintext);
+        // call encryption() function here
+        encryptedText = encryption(plaintext, key);
+        // call spacing() function here
+        encryptedText = spacing(encryptedText, spaces);
+        cout << "Ciphertext is: " + encryptedText << endl;
+    }
+
+}
+
+
